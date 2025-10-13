fadb73cccf1ca9949f655358124ad2f05045a397
Iqra <iqra.sami@ssasoft.com>
2025-10-13 23:52:08 +0500

    implemented edit profile api

--- src/api/user.ts
+++ src/api/user.ts
@@ -9,3 +9,23 @@ export const getUserProfile = async () => {
   });
   return response.data;
 };
+
+
+
+// update profile:
+interface UpdateProfileData {
+  id?: number;        // Add ID to payload
+  name?: string;      // Username field in backend
+  avatar?: string;    // Profile picture as Base64 or URL
+  email?: string;     // Optional if needed
+}
+
+export const updateUserProfile = async (data: UpdateProfileData) => {
+  const token = getAuthToken();
+  if (!token) throw new Error("No authentication token found. Please log in.");
+
+    const response = await apiClient.patch("/accounts/profile/", data, {
+      headers: { Authorization: `Token ${token}` },
+    });
+  return response.data;
+};
